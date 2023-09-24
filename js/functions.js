b40d4da3a8860388b0683e57d15742d25f75f875
m4ggiee <121451790+m4ggiee@users.noreply.github.com>
2023-09-24 19:16:55 +0300

    second commit

--- js/functions.js
+++ js/functions.js
@@ -73,11 +73,44 @@ function showAddUserForm() {
     parentSelector,
     '',
     {
+      name: 'age',
+      type: 'text',
+      placeholder: 'Enter your age'
+    }
+  ); // age input
+
+  createElement(
+    'input',
+    parentSelector,
+    '',
+    {
       name: 'email',
       type: 'text',
       placeholder: 'Enter email'
     }
   ); // email input
+
+  createElement(
+    'input',
+    parentSelector,
+    '',
+    {
+      name: 'phone',
+      type: 'text',
+      placeholder: 'Enter the phone number'
+    }
+  ); // phone number input
+
+  createElement(
+    'input',
+    parentSelector,
+    '',
+    {
+      name: 'bank',
+      type: 'text',
+      placeholder: 'Enter the bank account number'
+    }
+  ); // bank input
 
   createElement(
     'input',
@@ -100,14 +133,20 @@ function handleSaveUser() {
   const login = formElements.login.value;
   const name = formElements.name.value;
   const lastName = formElements.lastName.value;
+  const age = formElements.age.value;
   const email = formElements.email.value;
+  const phone = formElements.phone.value;
+  const bank = formElements.bank.value;
   const userId = parseInt(event.target.getAttribute('data-id'));
 
   const user = {
     login,
     name,
     lastName,
+    age,
     email,
+    phone,
+    bank,
     id: Date.now(), 
   };
 
@@ -129,7 +168,40 @@ function handleSaveUser() {
 }
 
 function validate(user) {
+  const loginRegex = /^[a-zA-Z0-9_]{3,20}$/; 
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; 
+  const nameRegex = /^[a-zA-Zа-яА-Я]+([- ][a-zA-Zа-яА-Я]+)*$/;
+  const lastNameRegex = /^[a-zA-Zа-яА-Я]+([- ][a-zA-Zа-яА-Я]+)*$/;
+  const phoneRegex = /^\+\d{1,3}\s?\(\d{1,3}\)\s?\d{3,}[-\s]?\d{2,}[-\s]?\d{2,}$/;
+  const ageRegex = /^(1[0-9]|[1-9])?[0-9]$/;
+  const bankRegex = /^\d{16}$/;
+  
+  if (!user.login.match(loginRegex)) {
+    return false; 
+  }
+  if (!user.phone.match(phoneRegex)) {
+    return false; 
+  }
+  if (!user.age.match(ageRegex)) {
+    return false; 
+  }
+  if (!user.bank.match(bankRegex)) {
+    return false; 
+  }
+  if (!user.name.match(nameRegex)) {
+    return false; 
+  }
+  if (!user.lastName.match(lastNameRegex)) {
+    return false; 
+  }
+
+  if (!user.email.match(emailRegex)) {
+    return false; 
+  }
+
+
   if (user.login === '' || user.email === '' || user.name === '' || user.lastName === '') {
+    
     return false;
   }
 
